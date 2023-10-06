ab75a7553d4d1ef7ed86c9f866640898b84200be
Jose Rafael Verduzco-Torres <JoseRafael.Verduzco-Torres@glasgow.ac.uk>
2023-10-06 22:03:16 +0100

    Include the accessibility function, which gets specific accessibility measures according to LSOA origin code

---
+++ R/accessibility.R
@@ -0,0 +1,67 @@
+#' Access the Accessibility Indicators Dataset for GB
+#'
+#' This function checks for the accessibility indicators dataset in the data directory,
+#' downloads it if necessary, and then loads the desired dataset based on the provided service and mode.
+#' The function can also subset the data based on provided origin locations.
+#'
+#' @param origins Vector of origin LSOA/DZ codes. If NULL (default), all locations are returned.
+#' @param service Character string specifying the desired service (e.g., "employment", "supermarket", "school"). Default is "employment".
+#' @param mode Character string specifying the transportation mode. Can be "public_transport" (default) or "car".
+#'
+#' @return A data frame containing the data from the desired dataset.
+#'
+#' @importFrom duckdb duckdb
+#' @importFrom DBI dbConnect dbGetQuery dbDisconnect
+#' @importFrom utils data
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#' accessibility(service = 'employment', mode = "public_transport")
+#' }
+accessibility <- function(origins = NULL, service = 'employment', mode = "public_transport") {
+  data_dir <- system.file("data", package = "AccessUK")
+
+  # Check if data directory exists and has data
+  if (!dir.exists(data_dir) || length(list.files(data_dir)) == 0) {
+    download_accessibility_data()
+  }
+
+  # Check mode
+  if(mode == "public_transport"){
+    mode <- "pt"
+  } else if (mode != "car"){
+    stop("Mode requested not available. It should be \"car\" or \"public_transport\"")
+  }
+
+  # List of files
+  all_files <- list.files(data_dir, recursive = TRUE, full.names = TRUE)
+  # Search file
+  access_file <- grep(mode, all_files, value = TRUE)
+  access_file <- grep(service, access_file, value = TRUE)
+
+  # Check if file exists
+  if (!file.exists(access_file)) {
+    stop("The desired indicator does not exist, choose from the modes and services available.")
+  }
+
+  # Read data using DuckDB
+  con <- DBI::dbConnect(duckdb::duckdb())
+
+  # Define if reading all the DF or just a subset
+  if(!is.null(origins)){
+    # Convert the vector to a comma-separated string
+    rows_string <- paste(sprintf("'%s'", origins), collapse = ",")
+    query <- paste0("SELECT * FROM '", access_file, "' WHERE geo_code in (", rows_string, ");")
+    access_indices <- DBI::dbGetQuery(con, query)
+  } else {
+    query <- paste0("SELECT * FROM '", access_file, ";")
+    access_indices <- DBI::dbGetQuery(con, query)
+  }
+
+  # Close connection
+  DBI::dbDisconnect(con, shutdown=TRUE)
+
+  # Return the data frame
+  return(access_indices)
+}
